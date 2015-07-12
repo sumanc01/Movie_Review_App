a0b3cdc0398e3e1edb3f9642480999be24a526b4
Suman Chaudhury <suman.chaudhury1@gmail.com>
2015-07-12 14:39:35 -0700

    Initial commit

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,59 @@
+class ReviewsController < ApplicationController
+  before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  before_action :set_movie
+
+  respond_to :html
+
+  def index
+    @reviews = Review.all
+    respond_with(@reviews)
+  end
+
+  def show
+    respond_with(@review)
+  end
+
+  def new
+    @review = Review.new
+    respond_with(@review)
+  end
+
+  def edit
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    @review.movie_id = @movie.id
+    
+    if @review.save
+      respond_with(@movie)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @review.update(review_params)
+    respond_with(@review)
+  end
+
+  def destroy
+    @review.destroy
+    respond_with(@review)
+  end
+
+  private
+    def set_review
+      @review = Review.find(params[:id])
+    end
+
+    def set_movie
+      @movie = Movie.find(params[:movie_id])
+
+    def review_params
+      params.require(:review).permit(:rating, :comment)
+    end
+  end
+end
